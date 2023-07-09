66e66ad30dcf8e8c59dc9bceb2e8b34a973b2b3d
Fernando Cardozo <nuevo_10000@outlook.com>
2023-07-09 16:39:38 -0400

    Ejercicio 3 secuencial terminado

--- Secuencia/Ejercicio 3/EjercicioEnC/Ejercicio3Secuencia.c
+++ Secuencia/Ejercicio 3/EjercicioEnC/Ejercicio3Secuencia.c
@@ -5,7 +5,20 @@
 */
 
 #include <stdio.h>
+#include <math.h>
 
 int main () {
+	// Declaración de variables.
+	float cateto1, cateto2, hipotenusa;
+	printf("Hipotenusa de un triángulo rectángulo");
+	printf("Ingrese el primer cateto: ");
+	// Entrada de datos.
+	scanf("%f", &cateto1);
+	printf("Ingrese el segundo cateto: ");
+	scanf("%f", &cateto2);
+	// Cálculos de la hipotenusa.
+	hipotenusa = sqrt(pow(cateto1, 2) + pow(cateto2, 2));
+	// Salida de datos.
+	printf("La hipotenusa de un triángulo rectángulo es: %.2f", hipotenusa);
 	return 0;
 }
